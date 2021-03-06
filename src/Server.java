bd969631306aabb03c019700ddd29f5c9ffb2668
Toothable <61289629+Toothable@users.noreply.github.com>
2021-03-07 01:03:09 +0800

    test

--- src/Server.java
+++ src/Server.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 public class Server {
 
     public static void main(String[] args) {
-
+        System.out.println("Toothable");
     }
 
     private static String parse(String str) {
